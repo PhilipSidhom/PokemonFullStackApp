bbe2fcb942cad7813d178a006999ffa7e0dfc7f5
PhilipSidhom <78218034+PhilipSidhom@users.noreply.github.com>
2023-03-30 16:54:17 -0400

    Update the Full Stack app

---
+++ pokemon-front-end/src/components/Home.js
@@ -0,0 +1,14 @@
+function Home() {
+    return (
+      <div className="Home">
+          <p>
+            Welcome to our Pokemon App
+          </p>
+          <a href = "http://localhost:1339/pokemons?Name=Pikatchu">Find Pikatchu</a>
+      </div>
+    );
+  }
+
+
+
+export default Home;
